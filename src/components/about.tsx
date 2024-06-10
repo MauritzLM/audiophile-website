b8295aae54d4957ec7d6ea92acd335aed358abfb
Mauritz Marais <mauritz303@gmail.com>
2024-06-10 16:35:39 +0200

    added component text and image

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,8 +1,22 @@
 
-function About(){
-    return (
-        <>
-          <h2>About section</h2>
-        </>
-    )
+function About() {
+  return (
+    <>
+      <section>
+        <picture>
+          <source media="(max-width: 700px)" srcSet="/src/assets/images/shared/mobile/image-best-gear.jpg" />
+          <source media="(max-width: 1000px)" srcSet="/src/assets/images/shared/tablet/image-best-gear.jpg" />
+          <source media="(min-width: 1001px)" srcSet="/src/assets/images/shared/desktop/image-best-gear.jpg" />
+          <img src="/src/assets/images/shared/desktop/image-best-gear.jpg" alt="" aria-hidden="true" decoding="async" width="" height="" loading="lazy" />
+        </picture>
+       
+        <h2>Bringing you the best audio gear</h2>
+        <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products.
+          Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+      </section>
+    </>
+  )
 }
+
+export default About
