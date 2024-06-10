56c5d84f07b14577affb5369d1b8a7d96c8923bc
Mauritz Marais <mauritz303@gmail.com>
2024-06-10 14:29:27 +0200

    types file

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+// item/product type
+export interface item {
+    id: number,
+    slug: string,
+    name: string,
+    image: {
+        mobile: string,
+        tablet: string,
+        desktop: string
+    },
+    category: string,
+    categoryImage: {
+        mobile: string,
+        tablet: string,
+        desktop: string
+    },
+    new: boolean,
+    price: number,
+    description: string,
+    features: string,
+    includes: {
+        quantity: number,
+        item: string
+    }[],
+    gallery: {
+        first: {
+            mobile: string,
+            tablet: string,
+            desktop: string
+        },
+        second: {
+            mobile: string,
+            tablet: string,
+            desktop: string
+        },
+        third: {
+            mobile: string,
+            tablet: string,
+            desktop: string
+        }
+    },
+    others: {
+        slug: string,
+        name: string,
+        image: {
+            mobile: string,
+            tablet: string,
+            desktop: string
+        }
+    }[],
+    featured: boolean
+}
+
+// fetched items types
+export interface fetchedItems {
+    [key: string]: item[],
+}
