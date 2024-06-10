950cf3164279cfc299d2185c096b4dc3172a9579
Mauritz Marais <mauritz303@gmail.com>
2024-06-10 14:33:26 +0200

    added proptypes

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,13 +1,39 @@
 import { useParams } from "react-router-dom"
+import { item, fetchedItems } from "../types";
+import ProductCardFull from "../components/products/productCardFull";
 
-function Product({products, updateCart}) {
-    const { productname } = useParams();
+interface productProps {
+    fetchedProducts: fetchedItems,
+    updateCart: (product: item, quantity: number) => void
+}
 
-    // import product card component*
+function Product({ fetchedProducts, updateCart }: productProps) {
+    // get category and product name from params
+    const { productname, category } = useParams();
+
+    // get product from state
+    const product = fetchedProducts[category!].find(item => item.name === productname);
+
+    // implement go back*
 
     return (
         <>
+            {/* go back */}
             <h1>{productname} page</h1>
+            {/* product card full component - update cart && product props */}
+            <ProductCardFull product={product!} updateCart={updateCart} />
+
+            {/* features */}
+
+            {/* in the box */}
+
+            {/* images */}
+
+            {/* other products component */}
+
+            {/* categories component */}
+
+            {/* about component */}
         </>
     )
 }
