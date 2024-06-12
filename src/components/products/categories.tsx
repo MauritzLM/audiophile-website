46026ea5c9d1f36bc75673798668028b963659cd
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 09:34:09 +0200

    added category images

--- src/components/products/categories.tsx
+++ src/components/products/categories.tsx
@@ -7,14 +7,17 @@ function Categories() {
     return (
         <>
             <div data-testid="category-link">
+                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-headphones.png" alt="headphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                 <p>headphones</p>
                 <Link to="/headphones">Shop</Link>
             </div>
             <div data-testid="category-link">
+                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-speakers.png" alt="speakers" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                 <p>speakers</p>
                 <Link to="/speakers">Shop</Link>
             </div>
             <div data-testid="category-link">
+                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-earphones.png" alt="earphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                 <p>earphones</p>
                 <Link to="/earphones">Shop</Link>
             </div>
