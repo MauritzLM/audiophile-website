7083c73a9a056d501100da297779272863738028
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 09:39:22 +0200

    added test ids

--- src/components/products/productFeatures.tsx
+++ src/components/products/productFeatures.tsx
@@ -7,30 +7,31 @@ interface productFeaturesProps {
 function ProductFeatures({ product }: productFeaturesProps) {
     return (
         <>
-            {/* features */}
             <h2>Features</h2>
-            <p>{product.description}</p>
+            <p data-testid="description">{product.description}</p>
+
             {/* in the box */}
             <ul>
                 {product.includes.map((item, index) => {
                     return <li key={`includes-${index}`}><span>{item.quantity}x</span> <span>{item.item}</span></li>
                 })}
             </ul>
+
             {/* images */}
             <div>
-                <picture>
+                <picture data-testid="picture">
                     <source media="(max-width: 700px)" srcSet={`/src/${product.gallery.first.mobile}`} />
                     <source media="(max-width: 1000px)" srcSet={`/src/${product.gallery.first.tablet}`} />
                     <source media="(min-width: 1001px)" srcSet={`/src/${product.gallery.first.desktop}`} />
                     <img src={`/src/${product.gallery.first.desktop}`} alt="building" aria-hidden="true" decoding="async" width="" height="" loading="lazy" />
                 </picture>
-                <picture>
+                <picture data-testid="picture">
                     <source media="(max-width: 700px)" srcSet={`/src/${product.gallery.second.mobile}`} />
                     <source media="(max-width: 1000px)" srcSet={`/src/${product.gallery.second.tablet}`} />
                     <source media="(min-width: 1001px)" srcSet={`/src/${product.gallery.second.desktop}`} />
                     <img src={`/src/${product.gallery.first.desktop}`} alt="building" aria-hidden="true" decoding="async" width="" height="" loading="lazy" />
                 </picture>
-                <picture>
+                <picture data-testid="picture">
                     <source media="(max-width: 700px)" srcSet={`/src/${product.gallery.third.mobile}`} />
                     <source media="(max-width: 1000px)" srcSet={`/src/${product.gallery.third.tablet}`} />
                     <source media="(min-width: 1001px)" srcSet={`/src/${product.gallery.third.desktop}`} />
