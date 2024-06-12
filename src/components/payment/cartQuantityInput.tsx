ee2d91314bd445536ea4d5539b59aec687220657
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 15:33:35 +0200

    implemented component with functionality

---
+++ src/components/payment/cartQuantityInput.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { item } from "../../types"
+
+interface cartQuantityInputProps {
+    product: item,
+    cart: item[],
+    updateCart: (product: item, quantity: number) => void
+}
+
+function CartQuantityInput({ product, cart, updateCart }: cartQuantityInputProps) {
+    // determine quantity
+    let quantity = 0;
+    cart.forEach(item => {
+        if (item.name === product.name) {
+            quantity++
+        }
+    });
+
+    const [inputValue, setInputValue] = useState(quantity);
+
+    const updateQuantity = (operator: string) => {
+        
+        if (operator === "remove") {
+            setInputValue(inputValue - 1)
+            updateCart(product, inputValue - 1)
+        }
+
+        if (operator === "add") {
+            setInputValue(inputValue + 1)
+            updateCart(product, inputValue + 1)
+        }
+    };
+    
+    return (
+        <>
+            <label htmlFor="cart-quantity">quantity
+                {/* remove button */}
+                <button className="remove-btn" onClick={() => updateQuantity("remove")}>-</button>
+
+                <input data-testid="cart-quantity" name="cart-quantity" id="cart-quantity" type="number" value={inputValue} onChange={(e) => { setInputValue(Number(e.target.value)) }} min="0" max="5" />
+                {/* add button */}
+                <button className="add-btn" onClick={() => updateQuantity("add")}>+</button>
+            </label>
+        </>
+    )
+}
+
+export default CartQuantityInput
