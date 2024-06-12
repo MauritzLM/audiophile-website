890802522b2470fece83bbb6866f41006b9c08c1
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 11:28:21 +0200

    added new product item

--- src/tests/mocks.ts
+++ src/tests/mocks.ts
@@ -1,90 +1,180 @@
 
 export const test_item = {
-    "id": 5,
-    "slug": "zx7-speaker",
-    "name": "ZX7 Speaker",
-    "image": {
-      "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-product.jpg",
-      "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-product.jpg",
-      "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-product.jpg"
+  "id": 5,
+  "slug": "zx7-speaker",
+  "name": "ZX7 Speaker",
+  "image": {
+    "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-product.jpg",
+    "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-product.jpg",
+    "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-product.jpg"
+  },
+  "category": "speakers",
+  "categoryimage": {
+    "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-category-page-preview.jpg",
+    "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-category-page-preview.jpg",
+    "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-category-page-preview.jpg"
+  },
+  "new": false,
+  "price": 3500,
+  "featured": false,
+  "description": "Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+  "features": "Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage.\n\nThe ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.",
+  "includes": [
+    {
+      "quantity": 2,
+      "item": "Speaker unit"
     },
-    "category": "speakers",
-    "categoryimage": {
-      "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-category-page-preview.jpg",
-      "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-category-page-preview.jpg",
-      "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-category-page-preview.jpg"
+    {
+      "quantity": 2,
+      "item": "Speaker cloth panel"
     },
-    "new": false,
-    "price": 3500,
-    "featured": false,
-    "description": "Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
-    "features": "Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage.\n\nThe ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.",
-    "includes": [
-      {
-        "quantity": 2,
-        "item": "Speaker unit"
-      },
-      {
-        "quantity": 2,
-        "item": "Speaker cloth panel"
-      },
-      {
-        "quantity": 1,
-        "item": "User manual"
-      },
-      {
-        "quantity": 1,
-        "item": "3.5mm 7.5m audio cable"
-      },
-      {
-        "quantity": 1,
-        "item": "7.5m optical cable"
-      }
-    ],
-    "gallery": {
-      "first": {
-        "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-1.jpg",
-        "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-1.jpg",
-        "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-1.jpg"
-      },
-      "second": {
-        "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-2.jpg",
-        "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-2.jpg",
-        "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-2.jpg"
-      },
-      "third": {
-        "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-3.jpg",
-        "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-3.jpg",
-        "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-3.jpg"
+    {
+      "quantity": 1,
+      "item": "User manual"
+    },
+    {
+      "quantity": 1,
+      "item": "3.5mm 7.5m audio cable"
+    },
+    {
+      "quantity": 1,
+      "item": "7.5m optical cable"
+    }
+  ],
+  "gallery": {
+    "first": {
+      "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-1.jpg",
+      "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-1.jpg",
+      "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-1.jpg"
+    },
+    "second": {
+      "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-2.jpg",
+      "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-2.jpg",
+      "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-2.jpg"
+    },
+    "third": {
+      "mobile": "/src/assets/images/product-zx7-speaker/mobile/image-gallery-3.jpg",
+      "tablet": "/src/assets/images/product-zx7-speaker/tablet/image-gallery-3.jpg",
+      "desktop": "/src/assets/images/product-zx7-speaker/desktop/image-gallery-3.jpg"
+    }
+  },
+  "others": [
+    {
+      "slug": "zx9-speaker",
+      "name": "ZX9 Speaker",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-zx9-speaker.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-zx9-speaker.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-zx9-speaker.jpg"
       }
     },
-    "others": [
-      {
-        "slug": "zx9-speaker",
-        "name": "ZX9 Speaker",
-        "image": {
-          "mobile": "/src/assets/images/shared/mobile/image-zx9-speaker.jpg",
-          "tablet": "/src/assets/images/shared/tablet/image-zx9-speaker.jpg",
-          "desktop": "/src/assets/images/shared/desktop/image-zx9-speaker.jpg"
-        }
-      },
-      {
-        "slug": "xx99-mark-one-headphones",
-        "name": "XX99 Mark I",
-        "image": {
-          "mobile": "/src/assets/images/shared/mobile/image-xx99-mark-one-headphones.jpg",
-          "tablet": "/src/assets/images/shared/tablet/image-xx99-mark-one-headphones.jpg",
-          "desktop": "/src/assets/images/shared/desktop/image-xx99-mark-one-headphones.jpg"
-        }
-      },
-      {
-        "slug": "xx59-headphones",
-        "name": "XX59",
-        "image": {
-          "mobile": "/src/assets/images/shared/mobile/image-xx59-headphones.jpg",
-          "tablet": "/src/assets/images/shared/tablet/image-xx59-headphones.jpg",
-          "desktop": "/src/assets/images/shared/desktop/image-xx59-headphones.jpg"
-        }
+    {
+      "slug": "xx99-mark-one-headphones",
+      "name": "XX99 Mark I",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-xx99-mark-one-headphones.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-xx99-mark-one-headphones.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-xx99-mark-one-headphones.jpg"
       }
-    ]
-  };
+    },
+    {
+      "slug": "xx59-headphones",
+      "name": "XX59",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-xx59-headphones.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-xx59-headphones.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-xx59-headphones.jpg"
+      }
+    }
+  ]
+};
+
+export const test_item_2 = {
+  "id": 4,
+  "slug": "xx99-mark-two-headphones",
+  "name": "XX99 Mark II Headphones",
+  "image": {
+    "mobile": "/src/assets/images/product-xx99-mark-two-headphones/mobile/image-product.jpg",
+    "tablet": "/src/assets/images/product-xx99-mark-two-headphones/tablet/image-product.jpg",
+    "desktop": "/src/assets/images/product-xx99-mark-two-headphones/desktop/image-product.jpg"
+  },
+  "category": "headphones",
+  "categoryimage": {
+    "mobile": "/src/assets/images/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg",
+    "tablet": "/src/assets/images/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg",
+    "desktop": "/src/assets/images/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
+  },
+  "new": true,
+  "price": 2999,
+  "featured": true,
+  "description": "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+  "features": "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat.\n\nThe advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
+  "includes": [
+    {
+      "quantity": 1,
+      "item": "Headphone unit"
+    },
+    {
+      "quantity": 2,
+      "item": "Replacement earcups"
+    },
+    {
+      "quantity": 1,
+      "item": "User manual"
+    },
+    {
+      "quantity": 1,
+      "item": "3.5mm 5m audio cable"
+    },
+    {
+      "quantity": 1,
+      "item": "Travel bag"
+    }
+  ],
+  "gallery": {
+    "first": {
+      "mobile": "/src/assets/images/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg",
+      "tablet": "/src/assets/images/product-xx99-mark-two-headphones/tablet/image-gallery-1.jpg",
+      "desktop": "/src/assets/images/product-xx99-mark-two-headphones/desktop/image-gallery-1.jpg"
+    },
+    "second": {
+      "mobile": "/src/assets/images/product-xx99-mark-two-headphones/mobile/image-gallery-2.jpg",
+      "tablet": "/src/assets/images/product-xx99-mark-two-headphones/tablet/image-gallery-2.jpg",
+      "desktop": "/src/assets/images/product-xx99-mark-two-headphones/desktop/image-gallery-2.jpg"
+    },
+    "third": {
+      "mobile": "/src/assets/images/product-xx99-mark-two-headphones/mobile/image-gallery-3.jpg",
+      "tablet": "/src/assets/images/product-xx99-mark-two-headphones/tablet/image-gallery-3.jpg",
+      "desktop": "/src/assets/images/product-xx99-mark-two-headphones/desktop/image-gallery-3.jpg"
+    }
+  },
+  "others": [
+    {
+      "slug": "xx99-mark-one-headphones",
+      "name": "XX99 Mark I",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-xx99-mark-one-headphones.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-xx99-mark-one-headphones.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-xx99-mark-one-headphones.jpg"
+      }
+    },
+    {
+      "slug": "xx59-headphones",
+      "name": "XX59",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-xx59-headphones.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-xx59-headphones.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-xx59-headphones.jpg"
+      }
+    },
+    {
+      "slug": "zx9-speaker",
+      "name": "ZX9 Speaker",
+      "image": {
+        "mobile": "/src/assets/images/shared/mobile/image-zx9-speaker.jpg",
+        "tablet": "/src/assets/images/shared/tablet/image-zx9-speaker.jpg",
+        "desktop": "/src/assets/images/shared/desktop/image-zx9-speaker.jpg"
+      }
+    }
+  ]
+};
