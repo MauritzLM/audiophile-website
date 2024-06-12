4b7a3f212936c2553c379fbadc40f1ce607f4589
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 13:08:16 +0200

    added remove and add button with functions

--- src/components/quantityInput.tsx
+++ src/components/quantityInput.tsx
@@ -8,12 +8,35 @@ interface quantityInputProps {
 
 function QuantityInput({ product, updateCart }: quantityInputProps) {
   const [inputValue, setInputValue] = useState(1)
-  
+
+  const lowerQuantity = function () {
+    if (inputValue === 1) {
+      return
+    } else {
+      setInputValue(inputValue - 1)
+    }
+  }
+
+  const increaseQuantity = function () {
+    if (inputValue === 5) {
+      return
+    } else {
+      setInputValue(inputValue + 1)
+    }
+  }
+
   return (
     <>
       <div>
         <label htmlFor="quantity">quantity</label>
-        <input name="quantity" id="quantity" type="number" value={inputValue} onChange={(e) => setInputValue(Number(e.target.value))} min="1" max="5"></input>
+        <div>
+          {/* remove button */}
+          <button data-testid="remove" className="remove-btn" onClick={lowerQuantity}>-</button>
+
+          <input name="quantity" id="quantity" type="number" value={inputValue} onChange={(e) => setInputValue(Number(e.target.value))} min="1" max="5"></input>
+          {/* add button */}
+          <button data-testid="add" className="add-btn" onClick={increaseQuantity}>+</button>
+        </div>
         <button onClick={() => updateCart(product, inputValue)}>add to cart</button>
       </div>
     </>
