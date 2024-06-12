0daccae3d1dc31a43c64d5a3362ef9e7a9583b7e
Mauritz Marais <mauritz303@gmail.com>
2024-06-12 09:39:49 +0200

    added check for images and description rendering

--- src/components/products/tests/productFeatures.test.tsx
+++ src/components/products/tests/productFeatures.test.tsx
@@ -12,6 +12,8 @@ describe("product features", () => {
         render(<ProductFeatures product={test_item}/>);
 
         expect(screen.getByRole("heading").textContent).toMatch(/Features/);
+        expect(screen.getByTestId("description")).toBeInTheDocument();
         expect(screen.getAllByRole("listitem")).toHaveLength(5);
+        expect(screen.getAllByTestId("picture")).toHaveLength(3);
     });
 });
