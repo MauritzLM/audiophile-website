4ba20de56530bbea46f1b99db11fe1c9c2159bad
Mauritz Marais <mauritz303@gmail.com>
2024-06-13 11:17:51 +0200

    added fetch product function if product not found in app state

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -5,6 +5,7 @@ import ProductFeatures from "../components/products/productFeatures";
 import OtherProducts from "../components/products/otherProducts";
 import Categories from "../components/products/categories";
 import About from "../components/about";
+import { useEffect, useState } from "react";
 
 interface productProps {
     fetchedProducts: fetchedItems,
@@ -15,8 +16,27 @@ function Product({ fetchedProducts, addToCart }: productProps) {
     // get category and product name from params
     const { productname, category } = useParams();
 
-    // get product from state
-    const product = fetchedProducts[category!].find(item => item.name === productname);
+    const [product, setProduct] = useState(fetchedProducts[category!].find(item => item.name === productname));
+
+    async function fetchProduct() {
+        try {
+            const response = await fetch(`http://localhost:3000/product/${productname}`);
+
+            const fetchedProduct = await response.json();
+
+            setProduct(fetchedProduct);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // if product not in state then fetch it and update state
+    useEffect(() => {
+        if (!product) {
+            fetchProduct();
+        }
+    });
 
     // implement go back*
 
@@ -29,7 +49,6 @@ function Product({ fetchedProducts, addToCart }: productProps) {
 
             {/* product features */}
             <ProductFeatures product={product!} />
-
 
             {/* other products component */}
             <OtherProducts product={product!} />
