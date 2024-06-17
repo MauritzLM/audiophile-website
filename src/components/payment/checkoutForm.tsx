31f57c35925b21aef1a4dc6d8f1372420fa4b670
Mauritz Marais <mauritz303@gmail.com>
2024-06-17 10:09:11 +0200

    added payment method conditional rendering

--- src/components/payment/checkoutForm.tsx
+++ src/components/payment/checkoutForm.tsx
@@ -7,10 +7,10 @@ interface checkoutFormProps {
     cart: item[]
 }
 
+
 function CheckoutForm({ cart }: checkoutFormProps) {
-    const [paymentMethod, setPaymentMethod] = useState("");
+    const [paymentMethod, setPaymentMethod] = useState("e-money");
     // errors state*
-    // payment method state*
     // error class to display errors*
     // on submit function*
     return (
@@ -69,16 +69,39 @@ function CheckoutForm({ cart }: checkoutFormProps) {
                             <span>Payment method</span>
                             <div>
                                 <label htmlFor="e-money">E-money</label>
-                                <input name="method" id="e-money" type="radio" />
+                                <input name="method" id="e-money" type="radio" value="e-money" onClick={(e) => setPaymentMethod((e.target as HTMLInputElement).value)} />
                             </div>
 
                             <div>
                                 <label htmlFor="cash">Cash on delivery</label>
-                                <input name="method" id="cash" type="radio" />
+                                <input name="method" id="cash" type="radio" value="cash" onClick={(e) => setPaymentMethod((e.target as HTMLInputElement).value)} />
                             </div>
 
-                            {/* payment method conditional rendering* */}
-                            
+
+                            {/* payment method conditional rendering */}
+                            {paymentMethod === 'e-money' ?
+                                // e-money selected
+                                <div className="payment-method">
+                                    <div>
+                                        <label htmlFor="e-money-num">e-money number</label>
+                                        <input id="e-money-num" name="e-money-num" type="number" />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="e-money-pin">e-money PIN</label>
+                                        <input id="e-money-pin" name="e-money-pin" type="number" />
+                                    </div>
+
+                                </div>
+                                :
+                                // cash selected
+                                <div className="payment-method">
+                                    <img src="/src/assets/images/checkout/icon-cash-on-delivery.svg" alt="" height="48" width="48" aria-hidden="true" loading="lazy" decoding="async"></img>
+                                    <p>The ‘Cash on Delivery’ option enables you to pay in cash when our delivery courier arrives at your residence.
+                                        Just make sure your address is correct so that your order will not be cancelled.</p>
+                                </div>
+                            }
+
+
                         </div>
                     </fieldset>
                 </div>
