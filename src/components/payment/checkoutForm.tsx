a98d0a076e8a028ca76ac03318e85530b753409e
Mauritz Marais <mauritz303@gmail.com>
2024-06-19 11:03:41 +0200

    changed errors to conditionally render

--- src/components/payment/checkoutForm.tsx
+++ src/components/payment/checkoutForm.tsx
@@ -8,7 +8,9 @@ interface checkoutFormProps {
 }
 
 // initial error state
-const initialFormState = { "name": "", "email": "", "phone": "", "address": "", "zipCode": "", "city": "", "country": "", "eMoneyNum": "", "eMoneyPin": "", "paymentMethod": "e-money" };
+const initialFormState = {
+    "name": "", "email": "", "phone": "", "address": "", "zipCode": "", "city": "", "country": "", "eMoneyNum": "", "eMoneyPin": "", "paymentMethod": "e-money"
+};
 
 
 function CheckoutForm({ cart }: checkoutFormProps) {
@@ -22,10 +24,7 @@ function CheckoutForm({ cart }: checkoutFormProps) {
     // on submit function*
     const handleSubmit = async function (event: React.FormEvent<HTMLFormElement>) {
         try {
-
             event.preventDefault();
-
-            console.log(formData);
 
             const response = await fetch('http://localhost:3000/payment/submit', {
                 method: 'POST',
@@ -37,7 +36,7 @@ function CheckoutForm({ cart }: checkoutFormProps) {
 
             const message = await response.json();
 
-            // if errors - update errors state
+            // if validation errors - update errors state with current errors
             if (message.errors) {
                 const currentErrors: formObj = {}
 
@@ -71,21 +70,21 @@ function CheckoutForm({ cart }: checkoutFormProps) {
                     <fieldset>
                         <legend>Billing Details</legend>
                         <div className={errors.name ? "error" : ''}>
-                            <span className="error-msg">{errors.name}</span>
+                            {errors.name && (<span role="alert" className="error-msg">{errors.name}</span>)}
                             <label htmlFor="name">Name</label>
-                            <input name="name" id="name" type="text" value={formData.name} onInput={(e) => setFormData({ ...formData, "name": e.currentTarget.value })} />
+                            <input name="name" id="name" aria-label="name" type="text" value={formData.name} onInput={(e) => setFormData({ ...formData, "name": e.currentTarget.value })} />
                         </div>
 
                         <div className={errors.email ? "error" : ''}>
-                            <span className="error-msg">{errors.email}</span>
+                            {errors.email && (<span role="alert" className="error-msg">{errors.email}</span>)}
                             <label htmlFor="email">Email address</label>
-                            <input name="email" id="email" type="email" value={formData.email} onInput={(e) => setFormData({ ...formData, "email": e.currentTarget.value })} />
+                            <input name="email" id="email" type="email" aria-label="email" value={formData.email} onInput={(e) => setFormData({ ...formData, "email": e.currentTarget.value })} />
                         </div>
 
                         <div className={errors.phone ? "error" : ''}>
-                            <span className="error-msg">{errors.phone}</span>
+                            {errors.phone && (<span role="alert" className="error-msg">{errors.phone}</span>)}
                             <label htmlFor="phone">Phone</label>
-                            <input name="phone" id="phone" type="tel" value={formData.phone} onInput={(e) => setFormData({ ...formData, "phone": e.currentTarget.value })} />
+                            <input name="phone" id="phone" type="tel" aria-label="phone number" value={formData.phone} onInput={(e) => setFormData({ ...formData, "phone": e.currentTarget.value })} />
                         </div>
 
                     </fieldset>
@@ -94,27 +93,27 @@ function CheckoutForm({ cart }: checkoutFormProps) {
                     <fieldset>
                         <legend>Shipping info</legend>
                         <div className={errors.address ? "error" : ''}>
-                            <span className="error-msg">{errors.address}</span>
+                            {errors.address && (<span role="alert" className="error-msg">{errors.address}</span>)}
                             <label htmlFor="address">address</label>
-                            <input name="address" id="address" type="text" value={formData.address} onInput={(e) => setFormData({ ...formData, "address": e.currentTarget.value })} />
+                            <input name="address" id="address" type="text" aria-label="address" value={formData.address} onInput={(e) => setFormData({ ...formData, "address": e.currentTarget.value })} />
                         </div>
 
                         <div className={errors.zipCode ? "error" : ''}>
-                            <span className="error-msg">{errors.zipCode}</span>
+                            {errors.zipCode && (<span role="alert" className="error-msg">{errors.zipCode}</span>)}
                             <label htmlFor="zipcode">ZIP code</label>
-                            <input name="zipcode" id="zipcode" type="tel" value={formData.zipCode} onInput={(e) => setFormData({ ...formData, "zipCode": e.currentTarget.value })} />
+                            <input name="zipcode" id="zipcode" type="tel" aria-label="zipcode" value={formData.zipCode} onInput={(e) => setFormData({ ...formData, "zipCode": e.currentTarget.value })} />
                         </div>
 
                         <div className={errors.city ? "error" : ''}>
-                            <span className="error-msg">{errors.city}</span>
+                            {errors.city && (<span role="alert" className="error-msg">{errors.city}</span>)}
                             <label htmlFor="city">city</label>
-                            <input name="city" id="city" type="text" value={formData.city} onInput={(e) => setFormData({ ...formData, "city": e.currentTarget.value })} />
+                            <input name="city" id="city" type="text" aria-label="city" value={formData.city} onInput={(e) => setFormData({ ...formData, "city": e.currentTarget.value })} />
                         </div>
 
                         <div className={errors.country ? "error" : ''}>
-                            <span className="error-msg">{errors.country}</span>
+                            {errors.country && (<span role="alert" className="error-msg">{errors.country}</span>)}
                             <label htmlFor="country">country</label>
-                            <input name="country" id="country" type="text" value={formData.country} onInput={(e) => setFormData({ ...formData, "country": e.currentTarget.value })} />
+                            <input name="country" id="country" type="text" aria-label="country" value={formData.country} onInput={(e) => setFormData({ ...formData, "country": e.currentTarget.value })} />
                         </div>
                     </fieldset>
 
@@ -140,14 +139,14 @@ function CheckoutForm({ cart }: checkoutFormProps) {
                                 // e-money selected
                                 <div className="payment-method">
                                     <div className={errors.eMoneyNum ? "error" : ''}>
-                                        <span className="error-msg">{errors.eMoneyNum}</span>
+                                        {errors.eMoneyNum && (<span role="alert" className="error-msg">{errors.eMoneyNum}</span>)}
                                         <label htmlFor="e-money-num">e-money number</label>
-                                        <input id="e-money-num" name="e-money-num" type="text" value={formData.eMoneyNum} onInput={(e) => setFormData({ ...formData, "eMoneyNum": e.currentTarget.value })} />
+                                        <input id="e-money-num" name="e-money-num" type="text" aria-label="e money number" value={formData.eMoneyNum} onInput={(e) => setFormData({ ...formData, "eMoneyNum": e.currentTarget.value })} />
                                     </div>
                                     <div className={errors.eMoneyPin ? "error" : ''}>
-                                        <span className="error-msg">{errors.eMoneyPin}</span>
+                                        {errors.eMoneyPin && (<span role="alert" className="error-msg">{errors.eMoneyPin}</span>)}
                                         <label htmlFor="e-money-pin">e-money PIN</label>
-                                        <input id="e-money-pin" name="e-money-pin" type="text" value={formData.eMoneyPin} onInput={(e) => setFormData({ ...formData, "eMoneyPin": e.currentTarget.value })} />
+                                        <input id="e-money-pin" name="e-money-pin" type="text" aria-label="e money pin" value={formData.eMoneyPin} onInput={(e) => setFormData({ ...formData, "eMoneyPin": e.currentTarget.value })} />
                                     </div>
 
                                 </div>
