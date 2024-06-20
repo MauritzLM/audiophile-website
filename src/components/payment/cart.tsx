c56334f32ff3b0a01c38882fd98199204b97202d
Mauritz Marais <mauritz303@gmail.com>
2024-06-20 13:10:15 +0200

    added viewcart prop

--- src/components/payment/cart.tsx
+++ src/components/payment/cart.tsx
@@ -5,10 +5,11 @@ import CartQuantityInput from "./cartQuantityInput";
 interface cartProps {
   cart: item[],
   updateCart: (product: item, quantity: number) => void,
-  clearCart: () => void
+  clearCart: () => void,
+  viewCart: boolean
 }
 
-function Cart({ cart, updateCart, clearCart }: cartProps) {
+function Cart({ cart, updateCart, clearCart, viewCart }: cartProps) {
 
   // array to keep track of unique items
   const compareArr: string[] = [];
@@ -19,47 +20,54 @@ function Cart({ cart, updateCart, clearCart }: cartProps) {
   if (!cart.length) {
     return (
       <>
-        <h2>Cart ({cart.length})</h2>
-        <p>cart is empty</p>
+        <div className={viewCart ? "cart-wrapper" : "hidden"}>
+          <div className="cart">
+            <h2>Cart ({cart.length})</h2>
+            <p>cart is empty</p>
+          </div>
+        </div>
       </>
     )
   }
 
   return (
     <>
-      {/* remove all */}
-      <button onClick={clearCart}>Remove all</button>
+      <div className={viewCart ? "cart-wrapper" : "hidden"}>
+        <div className="cart">
+          {/* remove all */}
+          <button onClick={clearCart}>Remove all</button>
 
-      <h2>Cart ({cart.length})</h2>
+          <h2>Cart ({cart.length})</h2>
 
-      <ul>
-        {/* for each unique item */}
-        {cartSorted.map((item) => {
-          // check if item is unique
-          if (compareArr.includes(item.name)) {
-            return
-          }
-          else {
-            compareArr.push(item.name)
+          <ul>
+            {/* for each unique item */}
+            {cartSorted.map((item) => {
+              // check if item is unique
+              if (compareArr.includes(item.name)) {
+                return
+              }
+              else {
+                compareArr.push(item.name)
 
-            return <li key={item.name}>
-              {/* image* name price */}
-              <h4>{item.name}</h4>
-              <p>{item.price}</p>
+                return <li key={item.name}>
+                  {/* image* name price */}
+                  <h4>{item.name}</h4>
+                  <p>{item.price}</p>
 
-              <CartQuantityInput product={item} cart={cart} updateCart={updateCart} />
-            </li>
-          }
-        })}
+                  <CartQuantityInput product={item} cart={cart} updateCart={updateCart} />
+                </li>
+              }
+            })}
 
-      </ul>
+          </ul>
 
-      {/* display total* */}
-      <div><span>Price</span><span data-testid="price">$ {cartSorted.reduce((accumulator, item) => accumulator + item.price, 0)}</span></div>
+          {/* display total* */}
+          <div><span>Price</span><span data-testid="price">$ {cartSorted.reduce((accumulator, item) => accumulator + item.price, 0)}</span></div>
 
-      {/* checkout link* */}
-      <Link to="/checkout">Checkout</Link>
-
+          {/* checkout link* */}
+          <Link to="/checkout">Checkout</Link>
+        </div>
+      </div>
     </>
   )
 }
