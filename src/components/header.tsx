2f5aa099e81459d3e7ec587a59d95475d17967f5
Mauritz Marais <mauritz303@gmail.com>
2024-06-20 13:07:26 +0200

    added cart view props and toggle button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,29 @@
 import { Link } from "react-router-dom"
 
-function Header() {
-    return (
-        <>
-          <header>
-             <nav>
+interface headerProps {
+   handleViewCart: () => void
+}
+
+function Header({ handleViewCart }: headerProps) {
+   return (
+      <>
+         <header>
+            <nav>
                {/* categories component for mobile nav* */}
-               
+
                {/* desktop links */}
-                <ul>
-                   <li><Link to="/">Home</Link></li>
-                   <li><Link to="/headphones">headphones</Link></li>
-                   <li><Link to="/speakers">speakers</Link></li>
-                   <li><Link to="/earphones">earphones</Link></li>
-                   <li><Link to="/checkout">checkout</Link></li>
-                </ul>
-                {/* cart display button* */}
-             </nav>
-          </header>
-        </>
-    )
+               <ul>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/headphones">headphones</Link></li>
+                  <li><Link to="/speakers">speakers</Link></li>
+                  <li><Link to="/earphones">earphones</Link></li>
+               </ul>
+               {/* cart display button* */}
+               <button onClick={handleViewCart}></button>
+            </nav>
+         </header>
+      </>
+   )
 }
 
 export default Header
