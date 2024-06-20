2aa5f33b42df84692e5ae073830a75b44743cc2e
Mauritz Marais <mauritz303@gmail.com>
2024-06-20 11:36:58 +0200

    added payment state and conditional rendering

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,18 +1,50 @@
 import { Link } from "react-router-dom"
+import { item } from "../types"
+import CheckoutForm from "../components/payment/checkoutForm"
+import { useState } from "react"
+import PaymentSuccess from "../components/payment/paymentSuccess"
 
-function Checkout({ cart }) {
+interface checkoutProps {
+    cart: item[],
+    clearCart: () => void
+}
+
+function Checkout({ cart, clearCart }: checkoutProps) {
+    const [paymentSuccess, setPaymentSuccess] = useState(false);
     // implement go back*
+    // payment success state* - pass to checkout form to update state*
+
+    const handlePayment = function () {
+        setPaymentSuccess(true)
+    }
 
     // render checkout form and order summary*
     // if paid render payment success component*
+
+
+    if (paymentSuccess) {
+        return (
+            // render checkout form*
+            // payment succes component z-index* background overlay*
+            <PaymentSuccess cart={cart} clearCart={clearCart}/>
+        )
+    }
+
+    if (!cart.length) {
+        return (
+            <>
+                <Link to="/">go back</Link>
+                <p>cart is empty</p>
+            </>
+        )
+    }
+
     return (
         <>
             <Link to="/">go back</Link>
-            <h1>Checkout page</h1>
             {/* form component* */}
 
-            {/* order summary component* */}
-
+            <CheckoutForm cart={cart} handlePayment={handlePayment} />
         </>
     )
 }
