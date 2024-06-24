85342031d8075f9b8227818eb6dafe07955b1471
Mauritz Marais <mauritz303@gmail.com>
2024-06-24 13:37:49 +0200

    implemented rendering test given prop

--- src/pages/tests/home.test.tsx
+++ src/pages/tests/home.test.tsx
@@ -1,10 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 import '@testing-library/jest-dom';
+import { BrowserRouter } from "react-router-dom";
+
+// component
+import Home from "../home";
+import { test_item, test_item_2 } from "../../tests/mocks";
+
+//mocks
+const featured = [test_item, test_item_2];
 
 // renders featured products
 describe("home page tests", () => {
-    it("rendering", () => {
-        
-    })
-})
+    it("renders all featured items", () => {
+        render(<BrowserRouter><Home featured={featured} /></BrowserRouter>)
+
+        const featuredProducts = screen.getAllByRole("listitem");
+
+        expect(featuredProducts).toHaveLength(2);
+    });
+});
