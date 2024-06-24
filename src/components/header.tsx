aa72a760439378ccc7a1b7a2eca824256b16ed61
Mauritz Marais <mauritz303@gmail.com>
2024-06-24 17:16:28 +0200

    added shownav state, mobile nav elements

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,42 @@
 import { Link } from "react-router-dom"
 import Categories from "./products/categories"
+import { useState } from "react"
 
 interface headerProps {
    handleViewCart: () => void
 }
 
 function Header({ handleViewCart }: headerProps) {
+   const [showNav, setShowNav] = useState(false)
    return (
       <>
-         <header>
+         <header id="cs-navigation" className={showNav ? "cs-active" : ""}>
             <nav>
-               {/* categories component for mobile nav */}
-               <div className="header-categories">
-                  <Categories />
-               </div>
+               {/* Mobile Nav Toggle */}
+               <button className={showNav ? "cs-toggle cs-active" : "cs-toggle"} aria-label="mobile menu toggle" onClick={() => showNav ? setShowNav(false) : setShowNav(true)}>
+                  <div className="cs-box" aria-hidden="true">
+                     <span className="cs-line cs-line1" aria-hidden="true"></span>
+                     <span className="cs-line cs-line2" aria-hidden="true"></span>
+                     <span className="cs-line cs-line3" aria-hidden="true"></span>
+                  </div>
+               </button>
 
-               {/* desktop links */}
-               <ul>
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/headphones">headphones</Link></li>
-                  <li><Link to="/speakers">speakers</Link></li>
-                  <li><Link to="/earphones">earphones</Link></li>
-               </ul>
+               {/* logo */}
+               <img src="/src/assets/svgs/logo.svg" alt="logo" width="143px" height="25px" decoding="async" loading="eager" />
+               {/* categories component for mobile nav */}
+               <div className="nav-links-wrapper">
+                  <div className="header-categories">
+                     <Categories />
+                  </div>
+
+                  {/* desktop links */}
+                  <ul>
+                     <li><Link to="/">Home</Link></li>
+                     <li><Link to="/headphones">headphones</Link></li>
+                     <li><Link to="/speakers">speakers</Link></li>
+                     <li><Link to="/earphones">earphones</Link></li>
+                  </ul>
+               </div>
 
                {/* cart display button* */}
                <button aria-label="cart" onClick={handleViewCart}><img src="/src/assets/svgs/icon-cart.svg" alt="cart" aria-hidden="true" decoding="async" loading="lazy" /></button>
