a41de959488cdc6a283ac2f685851a6567ecd640
Mauritz Marais <mauritz303@gmail.com>
2024-06-26 11:55:33 +0200

    added arrow image and container div

--- src/components/products/categories.tsx
+++ src/components/products/categories.tsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom"
+import "/src/assets/sass/components/categories.scss"
 
 function Categories() {
 
-    // uses images of new products*
-
     return (
         <>
-            <div data-testid="category-link">
-                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-headphones.png" alt="headphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
-                <p>headphones</p>
-                <Link to="/headphones">Shop</Link>
-            </div>
-            <div data-testid="category-link">
-                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-speakers.png" alt="speakers" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
-                <p>speakers</p>
-                <Link to="/speakers">Shop</Link>
-            </div>
-            <div data-testid="category-link">
-                <img src="/src/assets/images/shared/desktop/image-category-thumbnail-earphones.png" alt="earphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
-                <p>earphones</p>
-                <Link to="/earphones">Shop</Link>
+            <div className="categories">
+                <div className="category" data-testid="category-link">
+                    <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-headphones.png" alt="headphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
+                    <p>headphones</p>
+                    <Link to="/headphones">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
+                </div>
+                <div className="category" data-testid="category-link">
+                    <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-speakers.png" alt="speakers" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
+                    <p>speakers</p>
+                    <Link to="/speakers">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
+                </div>
+                <div className="category" data-testid="category-link">
+                    <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-earphones.png" alt="earphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
+                    <p>earphones</p>
+                    <Link to="/earphones">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
+                </div>
             </div>
         </>
     )
