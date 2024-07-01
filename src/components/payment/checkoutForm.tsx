a2244145c90ad2cb0159985c0daccd37bb964131
Mauritz Marais <mauritz303@gmail.com>
2024-07-01 10:22:08 +0200

    added classes to styling divs

--- src/components/payment/checkoutForm.tsx
+++ src/components/payment/checkoutForm.tsx
@@ -71,25 +71,25 @@ function CheckoutForm({ cart, handlePayment, paymentSuccess }: checkoutFormProps
 
     return (
         <>
-            <form aria-label="checkout form" name="checkout form" method="post" onSubmit={(e) => handleSubmit(e)}>
-                <div>
+            <form id="checkout-form" aria-label="checkout form" name="checkout form" method="post" onSubmit={(e) => handleSubmit(e)}>
+                <div className="form-wrapper">
                     <h2>Checkout</h2>
                     {/* billing details */}
                     <fieldset>
                         <legend>Billing Details</legend>
-                        <div className={errors.name ? "error" : ''}>
+                        <div className={errors.name ? "error form-group" : 'form-group'}>
                             {errors.name && (<span role="alert" className="error-msg">{errors.name}</span>)}
                             <label htmlFor="name">Name</label>
                             <input name="name" id="name" aria-label="name" type="text" value={formData.name} onInput={(e) => setFormData({ ...formData, "name": e.currentTarget.value })} placeholder="Alexei Ward" />
                         </div>
 
-                        <div className={errors.email ? "error" : ''}>
+                        <div className={errors.email ? "error form-group" : 'form-group'}>
                             {errors.email && (<span role="alert" className="error-msg">{errors.email}</span>)}
                             <label htmlFor="email">Email address</label>
                             <input name="email" id="email" type="email" aria-label="email" value={formData.email} onInput={(e) => setFormData({ ...formData, "email": e.currentTarget.value })} placeholder="alexei@mail.com" />
                         </div>
 
-                        <div className={errors.phone ? "error" : ''}>
+                        <div className={errors.phone ? "error form-group" : 'form-group'}>
                             {errors.phone && (<span role="alert" className="error-msg">{errors.phone}</span>)}
                             <label htmlFor="phone">Phone</label>
                             <input name="phone" id="phone" type="tel" aria-label="phone number" value={formData.phone} onInput={(e) => setFormData({ ...formData, "phone": e.currentTarget.value })} placeholder="+1 202-555-0136" />
@@ -100,25 +100,25 @@ function CheckoutForm({ cart, handlePayment, paymentSuccess }: checkoutFormProps
                     {/* shipping info */}
                     <fieldset>
                         <legend>Shipping info</legend>
-                        <div className={errors.address ? "error" : ''}>
+                        <div className={errors.address ? "error form-group" : 'form-group'}>
                             {errors.address && (<span role="alert" className="error-msg">{errors.address}</span>)}
                             <label htmlFor="address">address</label>
                             <input name="address" id="address" type="text" aria-label="address" value={formData.address} onInput={(e) => setFormData({ ...formData, "address": e.currentTarget.value })} placeholder="1137 Williams Avenue" />
                         </div>
 
-                        <div className={errors.zipCode ? "error" : ''}>
+                        <div className={errors.zipCode ? "error form-group" : 'form-group'}>
                             {errors.zipCode && (<span role="alert" className="error-msg">{errors.zipCode}</span>)}
                             <label htmlFor="zipcode">ZIP code</label>
                             <input name="zipcode" id="zipcode" type="tel" aria-label="zipcode" value={formData.zipCode} onInput={(e) => setFormData({ ...formData, "zipCode": e.currentTarget.value })} placeholder="10001" />
                         </div>
 
-                        <div className={errors.city ? "error" : ''}>
+                        <div className={errors.city ? "error form-group" : 'form-group'}>
                             {errors.city && (<span role="alert" className="error-msg">{errors.city}</span>)}
                             <label htmlFor="city">city</label>
                             <input name="city" id="city" type="text" aria-label="city" value={formData.city} onInput={(e) => setFormData({ ...formData, "city": e.currentTarget.value })} placeholder="New York" />
                         </div>
 
-                        <div className={errors.country ? "error" : ''}>
+                        <div className={errors.country ? "error form-group" : 'form-group'}>
                             {errors.country && (<span role="alert" className="error-msg">{errors.country}</span>)}
                             <label htmlFor="country">country</label>
                             <input name="country" id="country" type="text" aria-label="country" value={formData.country} onInput={(e) => setFormData({ ...formData, "country": e.currentTarget.value })} placeholder="United States" />
@@ -129,14 +129,14 @@ function CheckoutForm({ cart, handlePayment, paymentSuccess }: checkoutFormProps
                     <fieldset>
                         <legend>Payment Details</legend>
                         <div>
-                            <span>Payment method</span>
+                            <span className="payment-label">Payment method</span>
 
-                            <div>
+                            <div className="radio-group">
                                 <label htmlFor="e-money">E-money</label>
                                 <input className={paymentMethod === "e-money" ? "cs-active" : ""} name="method" id="e-money" type="radio" value="e-money" onChange={(e) => changeOption(e)} checked={paymentMethod === "e-money"} />
                             </div>
 
-                            <div>
+                            <div className="radio-group">
                                 <label htmlFor="cash">Cash on delivery</label>
                                 <input className={paymentMethod === "cash" ? "cs-active" : ""} name="method" id="cash" type="radio" value="cash" onChange={(e) => changeOption(e)} checked={paymentMethod === "cash"} />
                             </div>
@@ -146,12 +146,12 @@ function CheckoutForm({ cart, handlePayment, paymentSuccess }: checkoutFormProps
                             {paymentMethod === 'e-money' ?
                                 // e-money selected
                                 <div className="payment-method">
-                                    <div className={errors.eMoneyNum ? "error" : ''}>
+                                    <div className={errors.eMoneyNum ? "error form-group" : 'form-group'}>
                                         {errors.eMoneyNum && (<span role="alert" className="error-msg">{errors.eMoneyNum}</span>)}
                                         <label htmlFor="e-money-num">e-money number</label>
                                         <input id="e-money-num" name="e-money-num" type="text" aria-label="e money number" value={formData.eMoneyNum} onInput={(e) => setFormData({ ...formData, "eMoneyNum": e.currentTarget.value })} placeholder="238521993" />
                                     </div>
-                                    <div className={errors.eMoneyPin ? "error" : ''}>
+                                    <div className={errors.eMoneyPin ? "error form-group" : 'form-group'}>
                                         {errors.eMoneyPin && (<span role="alert" className="error-msg">{errors.eMoneyPin}</span>)}
                                         <label htmlFor="e-money-pin">e-money PIN</label>
                                         <input id="e-money-pin" name="e-money-pin" type="text" aria-label="e money pin" value={formData.eMoneyPin} onInput={(e) => setFormData({ ...formData, "eMoneyPin": e.currentTarget.value })} placeholder="6891" />
@@ -170,7 +170,7 @@ function CheckoutForm({ cart, handlePayment, paymentSuccess }: checkoutFormProps
                     </fieldset>
                 </div>
 
-                <div>
+                <div className="summary-wrapper">
                     <OrderSummary cart={cart} />
                     <button type="submit" disabled={paymentSuccess}>Continue & Pay</button>
                 </div>
