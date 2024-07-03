bc737fcec59a753712b3d60cbeb42171015170ee
Mauritz Marais <mauritz303@gmail.com>
2024-07-03 09:45:31 +0200

    changed category div to be a link element

--- src/components/products/categories.tsx
+++ src/components/products/categories.tsx
@@ -6,21 +6,23 @@ function Categories() {
     return (
         <>
             <div className="categories">
-                <div className="category" data-testid="category-link">
+                <Link to="/headphones" className="category" data-testid="category-link">
                     <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-headphones.png" alt="headphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                     <p>headphones</p>
-                    <Link to="/headphones">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
-                </div>
-                <div className="category" data-testid="category-link">
+                    <span>Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></span>
+                </Link>
+
+                <Link to="/speakers" className="category" data-testid="category-link">
                     <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-speakers.png" alt="speakers" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                     <p>speakers</p>
-                    <Link to="/speakers">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
-                </div>
-                <div className="category" data-testid="category-link">
+                    <span>Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></span>
+                </Link>
+                
+                <Link to="/earphones" className="category" data-testid="category-link">
                     <img className="category-image" src="/src/assets/images/shared/desktop/image-category-thumbnail-earphones.png" alt="earphones" height="" width="" loading="lazy" decoding="async" aria-hidden="true" />
                     <p>earphones</p>
-                    <Link to="/earphones">Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></Link>
-                </div>
+                    <span>Shop <img src="/src/assets/svgs/icon-arrow-right.svg" alt="" aria-hidden="true" decoding="async" loading="lazy" height="10px" width="5px" /></span>
+                </Link>
             </div>
         </>
     )
