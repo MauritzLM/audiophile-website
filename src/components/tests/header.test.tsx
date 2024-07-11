00748d59889c00ed910eeedf65bb4f918250b6dc
Mauritz Marais <mauritz303@gmail.com>
2024-07-11 12:27:37 +0200

    included cart prop

--- src/components/tests/header.test.tsx
+++ src/components/tests/header.test.tsx
@@ -8,12 +8,14 @@ import Header from "../header";
 import { BrowserRouter } from "react-router-dom";
 
 // mocks
+import { test_item } from "../../tests/mocks";
 const handleViewCart = vi.fn();
+const mock_cart = [test_item]
 
 describe("header test", () => {
     it("clicking on cart button calls viewcart function", async () => {
         
-        render(<BrowserRouter><Header handleViewCart={handleViewCart} /></BrowserRouter>);
+        render(<BrowserRouter><Header handleViewCart={handleViewCart} cart={mock_cart} /></BrowserRouter>);
 
         const user = userEvent.setup();
         const cartButton = screen.getByTestId("view-cart");
